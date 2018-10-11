59770755663f163a18c1ec8d92780870e0c0dafa
Dominique Orban <dominique.orban@gmail.com>
2018-10-10 20:47:51 -0400

    update to 0.7 and 1.0

--- src/HarwellRutherfordBoeing.jl
+++ src/HarwellRutherfordBoeing.jl
@@ -1,9 +1,11 @@
 module HarwellRutherfordBoeing
 
-using Compat
+using Printf
+using SparseArrays
 
 export HarwellBoeingMatrix, RutherfordBoeingData
 
+include("hrb_utils.jl")
 include("harwell_boeing.jl")
 include("rutherford_boeing.jl")
 
